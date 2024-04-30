df600a4728de095ff6a5421660c5bc7f282ffe2c
Krish Mittal <krishmittal212005@gmail.com>
2024-04-30 17:57:10 +0530

    Initial Commit

---
+++ HouseRizz-Seller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HouseRizz-Seller
+//
+//  Created by Krish Mittal on 30/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HouseRizz-Seller/HouseRizz_SellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseRizz_SellerApp.swift
+//  HouseRizz-Seller
+//
+//  Created by Krish Mittal on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct HouseRizz_SellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
