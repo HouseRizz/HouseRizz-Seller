511b3ea8d386f61d43115c1f946329d911940095
Krish Mittal <krishmittal212005@gmail.com>
2024-04-30 18:35:02 +0530

    login view

--- HouseRizz-Seller/View/LoginView.swift
+++ HouseRizz-Seller/View/LoginView.swift
@@ -19,52 +19,56 @@ struct LoginView: View {
     }
     
     var body: some View {
-        VStack {
-            VStack{
-                Text("HouseRizz")
-                    .bold()
-                    .font(.title)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.bottom,20)
-                
-                HStack {
-                    Image(systemName: "at")
-                    TextField("Email", text: $viewModel.email)
-                        .textInputAutocapitalization(.never)
-                        .disableAutocorrection(true)
-                        .submitLabel(.next)
-                    
-                }
-                .padding(.vertical, 6)
-                .background(Divider(), alignment: .bottom)
-                .padding(.bottom, 4)
-                
-                HStack {
-                    Image(systemName: "lock")
-                    SecureField("Password", text: $viewModel.password)
-                        .submitLabel(.go)
-                }
-                .padding(.vertical, 6)
-                .background(Divider(), alignment: .bottom)
-                .padding(.bottom, 8)
-                
-                if viewModel.authenticationState != .authenticating {
-                    HRButton(label: "Login") {
-                        signInWithEmailPassword()
-                    }
-                } else {
-                    ProgressView()
-                        .progressViewStyle(CircularProgressViewStyle(tint: .black))
-                        .padding(.vertical, 8)
-                        .frame(maxWidth: .infinity)
-                }
-            }
-            .padding()
+        VStack{
+            Image("icon")
+                .resizable()
+                .frame(width: 300,height: 300)
+                .padding(.top,50)
             
+            Spacer()
+            
+            Text("HouseRizz")
+                .bold()
+                .font(.title)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.bottom,20)
+            
+            HStack {
+                Image(systemName: "at")
+                TextField("Email", text: $viewModel.email)
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .submitLabel(.next)
+                
+            }
+            .padding(.vertical, 6)
+            .background(Divider(), alignment: .bottom)
+            .padding(.bottom, 4)
+            
+            HStack {
+                Image(systemName: "lock")
+                SecureField("Password", text: $viewModel.password)
+                    .submitLabel(.go)
+            }
+            .padding(.vertical, 6)
+            .background(Divider(), alignment: .bottom)
+            .padding(.bottom, 8)
+            
+            if viewModel.authenticationState != .authenticating {
+                HRButton(label: "Login") {
+                    signInWithEmailPassword()
+                }
+            } else {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: .black))
+                    .padding(.vertical, 8)
+                    .frame(maxWidth: .infinity)
+            }
+            
+            Spacer()
         }
         .listStyle(.plain)
         .padding()
-        .padding(.top,200)
     }
 }
 
