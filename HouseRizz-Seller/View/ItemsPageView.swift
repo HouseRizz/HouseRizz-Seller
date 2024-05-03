533e231b1aa91b5a25958855efea8df04cfd944f
Krish Mittal <krishmittal212005@gmail.com>
2024-05-03 16:44:58 +0530

    navigation

---
+++ HouseRizz-Seller/View/ItemsPageView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemsPageView.swift
+//  HouseRizz-Seller
+//
+//  Created by Krish Mittal on 03/05/24.
+//
+
+import SwiftUI
+
+struct ItemsPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemsPageView()
+}
---
+++ HouseRizz-Seller/View/OrdersPageView.swift
@@ -0,0 +1,18 @@
+//
+//  OrdersPageView.swift
+//  HouseRizz-Seller
+//
+//  Created by Krish Mittal on 03/05/24.
+//
+
+import SwiftUI
+
+struct OrdersPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrdersPageView()
+}
